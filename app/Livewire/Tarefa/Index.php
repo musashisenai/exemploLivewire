07e0c34b4d72b51486be66628f20e3a8285c48d8
musashisenai <musashisenai@gmail.com>
2026-02-10 15:34:46 -0300

    first commit

---
+++ app/Livewire/Tarefa/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire\Tarefa;
+
+use App\Models\Tarefa;
+use Livewire\Component;
+
+class Index extends Component
+{
+
+
+    public function render()
+    {
+        $tarefas = Tarefa::all();
+                
+        return view('livewire.tarefa.index', compact('tarefas'));
+    }
+}
